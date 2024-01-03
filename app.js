b000401d83ea8fda0b7a4a83642a6148b91545c7
Signet85 <tengis.ubtz@gmail.com>
2024-01-03 09:27:41 +0800

    Event listeneruudiig uusgej ugluu

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
+// Дэлгэцтэй ажиллах контроллер
 var uiController = (function(){
-    var x=100;
-    function add(y) {
-        return x+y;
-    }
-    return {
-            publicAdd: function(a){
-            a=add(a);
-            console.log('боловсруулсан утга '+a);
-        }
-    }
+ 
 
 })();
 
+// Санхүүтэй ажиллах контреллер
 var financeController = (function(){
-
+    
 })();
 
+// Програмын холбогч контреллер
 var appController = (function(uiController, financeController){
-    uiController.publicAdd(150);
+    
+    var ctrlAddItem = function(){
+        
+            //1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
+            console.log('дэлгэцээс өгөгдлөө авах');
+            //2. Олж авсан өгөгдлүүдээ санхүүгийн контреллерт дамжуулж тэнд хадгална.
+    
+            //3. Олж авсан өгөгдүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+    
+            //4.Төсвийг тооцоолно
+    
+            //5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+};
+    
+
+//Click дарахад үзүүлэх хариу үйлдэл
+    document.querySelector(".add__btn").addEventListener('click',  function(){
+    ctrlAddItem();
+    });
+    
+//Товч дарахад хариу өгөх функц
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode===13 || event.which === 13) {
+        ctrlAddItem();    
+        };
+    });
 })(uiController, financeController);

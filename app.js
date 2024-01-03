e89903b7aaacf3a41898d12720fee94ef6357f72
Signet85 <tengis.ubtz@gmail.com>
2024-01-03 10:42:59 +0800

    Finance module hiij ehelsen, income object uusgesen

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function(){
- 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn",
+        
+    };
 
+    
+    return{
+        getInput: function(){
+            return {
+            type: document.querySelector(DOMstrings.inputType).value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+        return DOMstrings;
+        }
+    };
 })();
 
 // Санхүүтэй ажиллах контреллер
 var financeController = (function(){
-    
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    var Expence = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    var data ={
+        allItems :{
+            inc: [],
+            exp: []
+        },
+        totals:{
+            inc:0,
+            exp:0
+        },
+    }
+
 })();
 
 // Програмын холбогч контреллер
@@ -15,7 +54,7 @@ var appController = (function(uiController, financeController){
     var ctrlAddItem = function(){
         
             //1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
-            console.log('дэлгэцээс өгөгдлөө авах');
+            console.log(uiController.getInput());
             //2. Олж авсан өгөгдлүүдээ санхүүгийн контреллерт дамжуулж тэнд хадгална.
     
             //3. Олж авсан өгөгдүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
@@ -23,18 +62,31 @@ var appController = (function(uiController, financeController){
             //4.Төсвийг тооцоолно
     
             //5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-};
+    };
     
 
 //Click дарахад үзүүлэх хариу үйлдэл
-    document.querySelector(".add__btn").addEventListener('click',  function(){
-    ctrlAddItem();
-    });
-    
-//Товч дарахад хариу өгөх функц
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode===13 || event.which === 13) {
-        ctrlAddItem();    
-        };
-    });
+    var setupEventListeners = function(){
+
+        var DOM = uiController.getDOMstrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click',  function(){
+            ctrlAddItem();
+            });
+        
+        //Товч дарахад хариу өгөх функц
+            document.addEventListener('keypress', function(event){
+                if(event.keyCode===13 || event.which === 13) {
+                ctrlAddItem();    
+                };
+            });
+    }
+    return {
+        init:function(){
+            console.log('application started...');
+            setupEventListeners();
+        }
+    }
 })(uiController, financeController);
+
+appController.init();
